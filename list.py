35bbb7e3f0e9832ab3dcb6d60eb433484026ecad
wangare <marywangareee@gmail.com>
2022-05-08 15:13:54 +0300

    first commit

---
+++ list.py
@@ -0,0 +1,18 @@
+from re import M
+
+
+my_list=['p','r','o','b','l','e','m']
+my_list[2:3]=[]
+print(my_list)
+print(my_list.pop())
+print(my_list.sort())
+print('k' in my_list)
+print('r' in my_list)
+
+numbers=[2,3,4,5,6,7]
+for x in numbers:print(x**5)
+  
+fruits=['apple','banana','mango','grapes']
+for fruit in fruits:
+    print("I like",fruit)
+   
